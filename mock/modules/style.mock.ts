1b21006b089c49201be033a05216cf09e94a5f16
wyj <cayww9@gmail.com>
2026-03-16 11:32:11 +0800

    fix: 1

--- mock/modules/style.mock.ts
+++ mock/modules/style.mock.ts
@@ -4,29 +4,33 @@
  */
 export default {
   /** 返回按钮背景图 */
-  backImage: 'https://huanniuchat.oss-accelerate.aliyuncs.com/template_development/sivio_back.png',
+  backImage: '/icons/back',
   /** 返回图片大小 */
   backImageSize: {
-    width: '24px',
-    height: '24px'
+    width: '40px',
+    height: '40px'
   },
   /** 顶部右侧举报图片 */
-  reportImage: 'https://huanniuchat.oss-accelerate.aliyuncs.com/template_development/sivio_report.png',
+  reportImage: '/icons/more',
   /** 举报图标大小 */
   reportImageSize: {
-    width: '24px', //图标宽度
-    height: '24px', //图标高度
+    width: '40px', // 图标宽度
+    height: '40px' // 图标高度
   },
   /** 点赞图标 */
-  likeIcon: 'https://huanniuchat.oss-accelerate.aliyuncs.com/template_development/sivio_like.png',
+  likeIcon: '/icons/liked',
 
   /** 点赞图标大小 */
   unlikeImageSize: {
-    width: '40px', //图标宽度
-    height: '40px', //图标高度
+    width: '36px', // 图标宽度
+    height: '36px' // 图标高度
   },
   /** 点赞数量文本样式 */
-  likeNumTextStyle: { fontSize: '12px', fontWeight: '400', color: 'rgba(255, 255, 255, 0.8)' },
+  likeNumTextStyle: {
+    fontSize: '12px',
+    fontWeight: '400',
+    color: 'rgba(255, 255, 255, 0.8)'
+  },
   /** 用户名文本样式 */
   userNameStyle: {
     fontSize: '16px',
@@ -63,9 +67,10 @@ export default {
     color: 'rgba(255, 255, 255, 0.8)',
     placeholderCorlor: 'rgba(255, 255, 255, 0.4)',
     /** 右侧发送图标 */
-    sendIcon: 'https://huanniuchat.oss-accelerate.aliyuncs.com/template_development/sivio_send_icon.png',
+    sendIcon:
+      'https://huanniuchat.oss-accelerate.aliyuncs.com/template_development/sivio_send_icon.png',
     sendIconWidth: '21px',
-    sendIconHeight: '21px',
+    sendIconHeight: '21px'
   },
   /** 表单标题样式 */
   formTitleStyle: {
@@ -102,7 +107,8 @@ export default {
   /** 高亮按钮样式 */
   highlightBtnStyle: {
     color: 'rgba(255, 255, 255, 1)',
-    backgroundColor: 'linear-gradient(151.23deg, rgba(71, 252, 255, 1) 0%, rgba(255, 94, 135, 1) 100%)'
+    backgroundColor:
+      'linear-gradient(151.23deg, rgba(71, 252, 255, 1) 0%, rgba(255, 94, 135, 1) 100%)'
   },
   /** 确认按钮样式 */
   confirmBtnStyle: {
@@ -120,7 +126,8 @@ export default {
     /** 发送背景色 */
     sendBgColor: '#434044',
     /** 接收背景色 */
-    receiveBgColor: 'linear-gradient(157.71deg, rgba(255, 94, 135, 1) 0%, rgba(71, 252, 255, 1) 100%)'
+    receiveBgColor:
+      'linear-gradient(157.71deg, rgba(255, 94, 135, 1) 0%, rgba(71, 252, 255, 1) 100%)'
   },
   /** 举报按钮 */
   reportBtnStyle: {
@@ -152,9 +159,10 @@ export default {
       width: '227px',
       height: '56px',
       color: 'rgba(255, 255, 255, 1)',
-      rightIcon: 'https://huanniuchat.oss-accelerate.aliyuncs.com/template_development/sivio_ai_chat_arr.png',
+      rightIcon:
+        'https://huanniuchat.oss-accelerate.aliyuncs.com/template_development/sivio_ai_chat_arr.png',
       rightIconWidth: '21px',
-      rightIconHeight: '21px',
+      rightIconHeight: '21px'
     },
     /** 弹框样式 */
     popupStyle: {
@@ -174,7 +182,8 @@ export default {
       bgImageStyle: {
         width: '280px',
         height: '210px',
-        bgImg: 'https://huanniuchat.oss-accelerate.aliyuncs.com/template_development/sivio_ai_pay_bg.png',
+        bgImg:
+          'https://huanniuchat.oss-accelerate.aliyuncs.com/template_development/sivio_ai_pay_bg.png'
       }
     }
   },
@@ -196,15 +205,18 @@ export default {
   shortVideoStyle: {
     backgroundColor: '#0e080f',
     /** 添加图标 */
-    addIcon: 'https://huanniuchat.oss-accelerate.aliyuncs.com/template_development/sivio_follow.png',
+    addIcon:
+      'https://huanniuchat.oss-accelerate.aliyuncs.com/template_development/sivio_follow.png',
     addIconWidth: '20px',
     addIconHeight: '20px',
     /** 留言图标 */
-    messageIcon: 'https://huanniuchat.oss-accelerate.aliyuncs.com/template_development/video_comments.png',
+    messageIcon:
+      'https://huanniuchat.oss-accelerate.aliyuncs.com/template_development/video_comments.png',
     messageIconWidth: '40px',
     messageIconHeight: '40px',
     /** 点赞图标 */
-    likeIcon: 'https://huanniuchat.oss-accelerate.aliyuncs.com/template_development/sivio_like_post.png',
+    likeIcon:
+      'https://huanniuchat.oss-accelerate.aliyuncs.com/template_development/sivio_like_post.png',
     avatarWidth: '48px',
     avatarHeight: '48px',
     /** 按钮样式 */
@@ -223,15 +235,18 @@ export default {
   otherHomeStyle: {
     backgroundColor: '#0e080f',
     /** 关注他人图标 */
-    addIcon: 'https://huanniuchat.oss-accelerate.aliyuncs.com/template_development/sivio_follow.png',
+    addIcon:
+      'https://huanniuchat.oss-accelerate.aliyuncs.com/template_development/sivio_follow.png',
     addIconWidth: '20px',
     addIconHeight: '20px',
     /** 留言图标 */
-    messageIcon: 'https://huanniuchat.oss-accelerate.aliyuncs.com/template_development/video_comments.png',
+    messageIcon:
+      'https://huanniuchat.oss-accelerate.aliyuncs.com/template_development/video_comments.png',
     messageIconWidth: '40px',
     messageIconHeight: '40px',
     /** 点赞图标 */
-    likeIcon: 'https://huanniuchat.oss-accelerate.aliyuncs.com/template_development/sivio_like_post.png',
+    likeIcon:
+      'https://huanniuchat.oss-accelerate.aliyuncs.com/template_development/sivio_like_post.png',
     likeIconWidth: '40px',
     likeIconHeight: '40px',
     /** 顶部数据样式 */
@@ -293,7 +308,8 @@ export default {
     backgroundColor: '#0e080f',
     /** 按钮文本样式 */
     btnTextStyle: {
-      backgroundColor: 'linear-gradient(136deg, rgba(255, 94, 135, 1) 60%, rgba(71, 252, 255, 1) 100%)',
+      backgroundColor:
+        'linear-gradient(136deg, rgba(255, 94, 135, 1) 60%, rgba(71, 252, 255, 1) 100%)',
       fontSize: '16px',
       fontWeight: '400',
       color: '#fff'
@@ -334,7 +350,8 @@ export default {
       height: '100px',
       borderRadius: '24px',
       backgroundColor: 'rgba(35, 30, 36, 1)',
-      selectedColor: 'linear-gradient(151.23deg, rgba(255, 94, 135, 1) 0%, rgba(48, 156, 133, 1) 100%)'
+      selectedColor:
+        'linear-gradient(151.23deg, rgba(255, 94, 135, 1) 0%, rgba(48, 156, 133, 1) 100%)'
     }
   }
 } satisfies Window['styleJson']
