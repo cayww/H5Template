0f5e55f5a78a137a9722c214ab3d2cc654f8bcd6
wyj <cayww9@gmail.com>
2026-03-20 13:49:38 +0800

    fix: 1

--- mock/modules/coins.mock.ts
+++ mock/modules/coins.mock.ts
@@ -1,33 +1,13 @@
 // 金币选项数据
 export default [
-  {
-    key: 'lvbsvhxcgcrvesor',
-    cions: 400,
-    meney: 0.99
-  },
-  {
-    key: 'dxismgcwewhrtezo',
-    cions: 2450,
-    meney: 4.99
-  },
-  {
-    key: 'khtxlcejaxmqcsra',
-    cions: 4900,
-    meney: 9.99
-  },
-  {
-    key: 'yadwwvxspgxwlndb',
-    cions: 9800,
-    meney: 19.99
-  },
-  {
-    key: 'qnrcuelbtiuflyky',
-    cions: 24500,
-    meney: 49.99
-  },
-  {
-    key: 'ymohxnvpkqxutvab',
-    cions: 49000,
-    meney: 99.99
-  }
+  { key: 'qathgzqkvpudnhyn', cions: 400, meney: 0.99 },
+  { key: 'duafsphdgogponwz', cions: 800, meney: 1.99 },
+  { key: 'kfjdslqwepzmxtya', cions: 1200, meney: 2.99 },
+  { key: 'avnpmwpxvquluvjh', cions: 2450, meney: 4.99 },
+  { key: 'amiheafsizsziwsg', cions: 5150, meney: 9.99 },
+  { key: 'illidzvnwnkqkezk', cions: 10800, meney: 19.99 },
+  { key: 'qplmznxvtyskwrah', cions: 15000, meney: 29.99 },
+  { key: 'kcsshxnugpymztvi', cions: 29400, meney: 49.99 },
+  { key: 'xmvqplratynskdwe', cions: 36000, meney: 69.99 },
+  { key: 'lgsezzfwkqfxynjc', cions: 63700, meney: 99.99 }
 ] as CoinInfo[]
